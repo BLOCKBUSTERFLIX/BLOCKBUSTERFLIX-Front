773f4c38e5e3a7484a758fa27c753ea820bffd99
Durann23 <jd43573@gmail.com>
2025-03-11 02:53:39 -0600

    sidebar de adminlte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', title: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), children: [
+            { path: '', loadComponent: () => import('./principal/principal.component').then(m => m.PrincipalComponent) },        
+        ]
+    }
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
