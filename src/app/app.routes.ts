496129882dba9eb05df45f620de5ad944524e583
WilbertACDX <115293547+WilbertACDX@users.noreply.github.com>
2025-03-12 20:50:37 +1300

    Not Found
    Error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
         path: '', title: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), children: [
             { path: 'crud/:table', loadComponent: () => import('./principal/principal.component').then(m => m.PrincipalComponent) },
         ]
-    }
+    },
+    { path: '**', title: 'Not-found', loadComponent: ()=> import('./error-404/error-404.component').then(c => c.Error404Component)}
 ];
